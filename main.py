92d4058fe52e0dbcc2b70a50b54e10ed30cdd344
b12901100 <167604844+b12901100@users.noreply.github.com>
2024-04-20 17:40:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import numpy as np
 import pandas
 
 from BTinterface import BTInterface
-from maze import Action, Maze
-from score import Scoreboard, ScoreboardFake
+#from maze import Action, Maze
+#from score import Scoreboard, ScoreboardFake
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
@@ -18,31 +18,34 @@ logging.basicConfig(
 log = logging.getLogger(__name__)
 
 # TODO : Fill in the following information
-TEAM_NAME = "huh?"
-SERVER_URL = "http://140.112.175.18:5000/"
-MAZE_FILE = "data/small_maze.csv"
-BT_PORT = "COM6"
-Mode = "1"
+#TEAM_NAME = "YOUR_TEAM_NAME"
+#SERVER_URL = "http://140.112.175.18:5000/"
+#MAZE_FILE = "data/small_maze.csv"
+BT_PORT = "COM8"
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mode", default=Mode, help="0: treasure-hunting, 1: self-testing", type=str)
-    parser.add_argument("--maze-file", default=MAZE_FILE, help="Maze file", type=str)
+    parser.add_argument("mode", help="0: treasure-hunting, 1: self-testing", type=str)
+    #parser.add_argument("--maze-file", default=MAZE_FILE, help="Maze file", type=str)
     parser.add_argument("--bt-port", default=BT_PORT, help="Bluetooth port", type=str)
-    parser.add_argument(
-        "--team-name", default=TEAM_NAME, help="Your team name", type=str
-    )
-    parser.add_argument("--server-url", default=SERVER_URL, help="Server URL", type=str)
+    #parser.add_argument(
+        #"--team-name", default=TEAM_NAME, help="Your team name", type=str
+    #)
+    
+    #parser.add_argument("--server-url", default=SERVER_URL, help="Server URL", type=str)
     return parser.parse_args()
 
 
-def main(mode: int, bt_port: str, team_name: str, server_url: str, maze_file: str):
-    maze = Maze(maze_file)
-    point = Scoreboard(team_name, server_url)
+def main(mode: int, bt_port: str):
+    # team_name: str, server_url: str, maze_file: str
+    #maze = Maze(maze_file)
+    #point = Scoreboard(team_name, server_url)
     # point = ScoreboardFake("your team name", "data/fakeUID.csv") # for local testing
     interface = BTInterface(port=bt_port)
     # TODO : Initialize necessary variables
-
+    i=0
+    action = ["l","b","s","b","r","b","s"]
     if mode == "0":
         log.info("Mode 0: For treasure-hunting")
         # TODO : for treasure-hunting, which encourages you to hunt as many scores as possible
@@ -50,21 +53,9 @@ def main(mode: int, bt_port: str, team_name: str, server_url: str, maze_file: st
     elif mode == "1":
         log.info("Mode 1: Self-testing mode.")
         # TODO: You can write your code to test specific function.
-        time.sleep(3)
-        interface.start()
-        while True:
-            msgreceive = interface.bt.serial_read_string()
-            if msgreceive == "node":
-                #TODO:
-                #if car arrive node
-                #tell car next step
-                interface.bt.serial_write_string(f"")
-            else :
-                point.add_UID(msgreceive)
-            if not interface.bt.do_connect(bt_port):#need another condition to break
-                break
-        time.sleep(1)
-        log.info(f"{point.get_current_score()}")
+        
+        while True:  
+            interface.read(action,i)
     else:
         log.error("Invalid mode")
         sys.exit(1)
